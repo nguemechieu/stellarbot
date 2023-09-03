5506e6c5570b12ac8efc7bfab6bfe66f26c695cf
Noel M Nguemechieu <NGUEMECHIEU@LIVE.COM>
2023-09-03 16:48:32 +0000

    ne1

--- orders.py
+++ orders.py
@@ -1,7 +1,17 @@
-from tkinter import Frame
+from tkinter import N,S,W,E, Frame, Label
 
 
 class Orders(Frame):
 
+    def __init__(self, parent, controller):
+        Frame.__init__(self, parent)
+        self.parent = parent
+        self.controller = controller
+        self.grid()
+        self.createWidgets()
     
-    
+    def createWidgets(self):
+
+
+        self.label = Label(self, text="Orders")
+        self.label.grid(row=0, column=0, columnspan=2, sticky=N+S+E+W)
